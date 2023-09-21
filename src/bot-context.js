4e391507e93f428cbc14a0cc21d9a3c32df8d10e
HeadMad <kov.ps@ya.ru>
2023-09-21 20:49:05 +0800

    add jsdoc to bot-context

--- src/bot-context.js
+++ src/bot-context.js
@@ -1,11 +1,25 @@
 import EventContext from './event-context.js';
-import {BotContextError} from './errors.js';
+import { BotContextError } from './errors.js';
 
-export default function(request) {
+export default BotContext;
+
+/**
+ * Creates a new BotContext object that handles events and provides a proxy to interact with the bot.
+ *
+ * @param {function} request - The request function for sending requests to telegram servers by token.
+ * @return {BotContext} A new BotContext object.
+ */
+function BotContext(request) {
   const EVENTS = new Map();
 
   const self = {};
 
+  /**
+   * Adds an event handler for the specified event name.
+   *
+   * @param {string|function} eventName - The name of the event or a function to handle all events.
+   * @param {function} eventHandler - The function to handle the event.
+   */
   self.on = (eventName, eventHandler) => {
     if (typeof eventName === 'function') {
       eventHandler = eventName;
@@ -18,6 +32,12 @@ export default function(request) {
       EVENTS.set(eventName, [eventHandler]);
   };
 
+/**
+ * Processes the given event payload and returns an array of event handlers.
+ *
+ * @param {Object} eventPayload - The payload of the event to be processed.
+ * @return {Array} An array of event handlers.
+ */
   self.process = (eventPayload) => {
     const eventName = Object.keys(eventPayload).find(key => key !== 'update_id');
     const handlers = [
@@ -26,15 +46,23 @@ export default function(request) {
     ];
     return handlers;
   };
-  
-function runEventHandlers(trigger, eventName, eventPayload) {
+
+/**
+ * Executes event handlers based on the given trigger, event name, and event payload.
+ *
+ * @param {string} trigger - The trigger that determines which event handlers to execute.
+ * @param {string} eventName - The name of the event.
+ * @param {any} eventPayload - The payload of the event.
+ * @return {Array} An array containing the results of executing the event handlers.
+ */
+  function runEventHandlers(trigger, eventName, eventPayload) {
     const result = [];
     if (EVENTS.has(trigger)) {
       const eventContext = EventContext(request, eventName, eventPayload);
-      for (let handler of EVENTS.get(trigger)) 
+      for (let handler of EVENTS.get(trigger))
         result.push(handler(eventContext, eventName));
     }
-    
+
     return result;
   }
 
@@ -50,16 +78,12 @@ function runEventHandlers(trigger, eventName, eventPayload) {
     set(target, prop, value) {
       if (prop in target)
         throw new BotContextError(`Can't rewrite method "${prop}"`);
-      
-      if (typeof prop !== 'function')
-      throw new BotContextError(`New method "${prop}" must be a function`);
 
-      target[prop]  = value;
+      if (typeof prop !== 'function')
+        throw new BotContextError(`New method "${prop}" must be a function`);
+
+      target[prop] = value;
       return true;
     },
-
-    apply(target, thisArg, args) {
-      return target.process(...args);
-    }
   });
 }
