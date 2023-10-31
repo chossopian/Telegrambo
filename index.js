44f59f51b8abf9ff4670190806843aa71d0980e5
Kovalenko Pavel <kov.ps@ya.ru>
2023-11-01 02:45:22 +0800

    remove dependency request-sync create default expor from nodeBotAsync

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import nodeBotAsync from './src/node-bot-async.js'
+
+export default nodeBotAsync;
+export { nodeBotAsync };
+
+// export { default as nodeBotSync } from './src/node-bot-sync.js';
+export { default as botContext } from './src/bot-context.js';
 export { default as browserBotSync } from './src/browser-bot-sync.js';
 export { default as browserBotAsync } from './src/browser-bot-async.js';
-export { default as nodeBotSync } from './src/node-bot-sync.js';
-export { default as nodeBotAsync } from './src/node-bot-async.js';
-export { default as botContext } from './src/bot-context.js';
